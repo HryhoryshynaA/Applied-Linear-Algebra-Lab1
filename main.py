f8517f515a48552b633c7b298c2cb4a63f90c1f2
Adriana Hryhoryshyna <ahryhoryshyna@kse.org.ua>
2024-05-22 12:27:35 +0300

    rotation method

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def rotate(figure, angle_degree):
+    angle_rad = np.radians(angle_degree)
+
+    rotation_matrix = np.array([[np.cos(angle_rad), -np.sin(angle_rad)], [np.sin(angle_rad), np.cos(angle_rad)]])
+    rotated_points = np.dot(figure, rotation_matrix)
+
+    return rotated_points
 
 batman = np.array([[0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4], [0, 0.8], [-0.5, 0.4], [-0.4, 1], [-1, 0.2], [0, 0]])
 triangle = np.array([[2, 4], [1, 1], [3, 1], [2, 4]])
 rectangle = np.array([[1, 3], [1, 1], [4, 1], [4, 3], [1, 3]])
-x = triangle[:, 0]
-y = triangle[:, 1]
+rotated_batman = rotate(batman, 90)
+x = rotated_batman[:, 0]
+y = rotated_batman[:, 1]
 
 plt.figure(figsize=(8, 6))
 plt.plot(x, y, marker='o', linestyle='-')
