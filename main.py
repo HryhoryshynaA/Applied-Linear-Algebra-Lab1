d0b434186d6b6a6bd5c95455243970598e586edb
Adriana Hryhoryshyna <ahryhoryshyna@kse.org.ua>
2024-05-23 10:31:49 +0300

    reflection method

--- main.py
+++ main.py
@@ -13,13 +13,26 @@ def scale(figure, coefficient):
     scaled_points = figure*coefficient
     return scaled_points
 
+def reflect(figure, axis):
+    if axis == "x":
+       reflect_points = np.array([[1, 0], [0, -1]])
+    elif axis == "y":
+        reflect_points = np.array([[-1, 0], [0, 1]])
+    elif axis == "origin":
+       reflect_points = np.array([[0, 0]]) - figure
+    else:
+        print("This type of reflextion is not implemented")
+
+    reflected_points = np.dot(figure, reflect_points)
+    return reflected_points
+
 batman = np.array([[0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4], [0, 0.8], [-0.5, 0.4], [-0.4, 1], [-1, 0.2], [0, 0]])
 triangle = np.array([[2, 4], [1, 1], [3, 1], [2, 4]])
 rectangle = np.array([[1, 3], [1, 1], [4, 1], [4, 3], [1, 3]])
-rotated_batman = rotate(batman, 90)
+reflected_triangle = reflect(triangle, "origin")
 scaled_triangle = scale(triangle, 12)
-x = scaled_triangle[:, 0]
-y = scaled_triangle[:, 1]
+x = triangle[:, 0]
+y = triangle[:, 1]
 
 plt.figure(figsize=(8, 6))
 plt.plot(x, y, marker='o', linestyle='-')
