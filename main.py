146c3dc5ccc91acb963adfb0f0eea6693a684231
Adriana Hryhoryshyna <ahryhoryshyna@kse.org.ua>
2024-05-27 15:25:58 +0300

    custom matrix and work with axis movement

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def rotate(figure, angle_degree):
     rotation_matrix = np.array([[np.cos(angle_rad), -np.sin(angle_rad)], [np.sin(angle_rad), np.cos(angle_rad)]])
     rotated_points = np.dot(figure, rotation_matrix)
 
-    return rotated_points
+    return rotated_points, rotation_matrix
 
 def scale(figure, coefficient):
     scaled_points = figure*coefficient
@@ -26,13 +26,34 @@ def reflect(figure, axis):
     reflected_points = np.dot(figure, reflect_points)
     return reflected_points
 
+def nahyl_axis(figure, axis, factor):
+    if axis == 'x':
+        new_matrix = np.array([[1, factor], [0, 1]])
+    elif axis == 'y':
+        new_matrix = np.array([[1, 0], [factor, 1]])
+    else:
+        print("Axis must be 'x' or 'y'")
+
+    new_points = np.dot(figure, new_matrix)
+    return new_points
+
+def custom_transform(figure, transformation_matrix):
+    transformed_points = np.dot(figure, transformation_matrix)
+    return transformed_points
+
 batman = np.array([[0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4], [0, 0.8], [-0.5, 0.4], [-0.4, 1], [-1, 0.2], [0, 0]])
 triangle = np.array([[2, 4], [1, 1], [3, 1], [2, 4]])
 rectangle = np.array([[1, 3], [1, 1], [4, 1], [4, 3], [1, 3]])
-reflected_triangle = reflect(triangle, "origin")
+
+random_transformation_matrix = np.array([[0.65, -0.6], [0, 1]])
+
+rotated_batman, rotate_matrix = rotate(batman, 45)
 scaled_triangle = scale(triangle, 12)
-x = triangle[:, 0]
-y = triangle[:, 1]
+nahyl_batman = nahyl_axis(batman, "x", 3)
+print("Rotation matrix:\n", rotate_matrix)
+transformed_batman = custom_transform(batman, random_transformation_matrix)
+x = transformed_batman[:, 0]
+y = transformed_batman[:, 1]
 
 plt.figure(figsize=(8, 6))
 plt.plot(x, y, marker='o', linestyle='-')
