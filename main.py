d09ee11825df2d4118db61a3d08d63edc9bb1ae3
Adriana Hryhoryshyna <ahryhoryshyna@kse.org.ua>
2024-06-01 22:50:28 +0300

    3d added and returning of matrixes in nthe mthods

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
+
 
 def rotate(figure, angle_degree):
     angle_rad = np.radians(angle_degree)
@@ -11,62 +11,46 @@ def rotate(figure, angle_degree):
     return rotated_points, rotation_matrix
 
 def scale(figure, coefficient):
-    scaled_points = figure*coefficient
-    return scaled_points
+    scale_matrix = np.array([[coefficient, 0], [0, coefficient]])
+    scaled_points = np.dot(figure, scale_matrix)
+    return scaled_points, scale_matrix
 
 def reflect(figure, axis):
     if axis == "x":
-       reflect_points = np.array([[1, 0], [0, -1]])
+        reflect_matrix = np.array([[1, 0], [0, -1]])
     elif axis == "y":
-        reflect_points = np.array([[-1, 0], [0, 1]])
+        reflect_matrix = np.array([[-1, 0], [0, 1]])
     elif axis == "origin":
-       reflect_points = np.array([[0, 0]]) - figure
+        reflect_matrix = -np.eye(2)
     else:
-        print("This type of reflextion is not implemented")
-
-    reflected_points = np.dot(figure, reflect_points)
-    return reflected_points
+        print("This type of reflection is not implemented")
+        return figure, None
+    reflected_points = np.dot(figure, reflect_matrix)
+    return reflected_points, reflect_matrix
 
 def nahyl_axis(figure, axis, factor):
     if axis == 'x':
-        new_matrix = np.array([[1, factor], [0, 1]])
+        shear_matrix = np.array([[1, factor], [0, 1]])
     elif axis == 'y':
-        new_matrix = np.array([[1, 0], [factor, 1]])
+        shear_matrix = np.array([[1, 0], [factor, 1]])
     else:
         print("Axis must be 'x' or 'y'")
-
-    new_points = np.dot(figure, new_matrix)
-    return new_points
+        return figure, None
+    sheared_points = np.dot(figure, shear_matrix)
+    return sheared_points, shear_matrix
 
 def custom_transform(figure, transformation_matrix):
     transformed_points = np.dot(figure, transformation_matrix)
-    return transformed_points
+    return transformed_points, transformation_matrix
 
-batman = np.array([[0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4], [0, 0.8], [-0.5, 0.4], [-0.4, 1], [-1, 0.2], [0, 0]])
 triangle = np.array([[2, 4], [1, 1], [3, 1], [2, 4]])
 rectangle = np.array([[1, 3], [1, 1], [4, 1], [4, 3], [1, 3]])
-pyramid = np.array([
-    [0, 0, 0],
-    [1, 0, 0],
-    [1, 1, 0],
-    [0, 1, 0],
-    [0.5, 0.5, 1]
-])
 
 random_transformation_matrix = np.array([[0.65, -0.6], [0, 1]])
-
-np.random.seed(42)
-random_transformation_matrix_3d = np.random.rand(3, 3)
-transformed_pyramid = custom_transform(pyramid, random_transformation_matrix_3d)
-
-
-rotated_batman, rotate_matrix = rotate(batman, 45)
 scaled_triangle = scale(triangle, 12)
-nahyl_batman = nahyl_axis(batman, "x", 3)
-print("Rotation matrix:\n", rotate_matrix)
-transformed_batman = custom_transform(batman, random_transformation_matrix)
-x = transformed_batman[:, 0]
-y = transformed_batman[:, 1]
+nahyl_rectangle = nahyl_axis(rectangle, "x", 3)
+x = scaled_triangle[:, 0]
+y = nahyl_rectangle[:, 1]
 
 plt.figure(figsize=(8, 6))
 plt.plot(x, y, marker='o', linestyle='-')
@@ -77,16 +61,25 @@ plt.axis('equal')
 plt.show()
 
 
+
+# 3D figures
+pyramid = np.array([
+    [0, 0, 0],
+    [1, 0, 0],
+    [1, 1, 0],
+    [0, 1, 0],
+    [0.5, 0.5, 1]
+])
+
+random_transformation_matrix_3d = np.random.rand(3, 3)
+transformed_pyramid = custom_transform(pyramid, random_transformation_matrix_3d)
 fig = plt.figure(figsize=(12, 6))
-
-
-edges = [
+pyramid_edges = [
     [0, 1], [1, 2], [2, 3], [3, 0],
     [0, 4], [1, 4], [2, 4], [3, 4]
 ]
-
 ax1 = fig.add_subplot(121, projection='3d')
-for edge in edges:
+for edge in pyramid_edges:
     ax1.plot([pyramid[edge[0], 0], pyramid[edge[1], 0]],
              [pyramid[edge[0], 1], pyramid[edge[1], 1]],
              [pyramid[edge[0], 2], pyramid[edge[1], 2]],)
@@ -97,7 +90,7 @@ ax1.set_zlabel("Z")
 
 
 ax2 = fig.add_subplot(122, projection='3d')
-for edge in edges:
+for edge in pyramid_edges:
     ax2.plot([transformed_pyramid[edge[0], 0], transformed_pyramid[edge[1], 0]],
              [transformed_pyramid[edge[0], 1], transformed_pyramid[edge[1], 1]],
              [transformed_pyramid[edge[0], 2], transformed_pyramid[edge[1], 2]],)
@@ -107,9 +100,4 @@ ax2.set_ylabel("Y")
 ax2.set_zlabel("Z")
 
 plt.show()
-
-print("Random Transformation Matrix:\n", random_transformation_matrix)
-
-
-
 
